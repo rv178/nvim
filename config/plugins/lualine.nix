9445ffd1bcc73a2fcf60c0baaa4a0d5ca7516b66
Rohith Vinod <idliyout@gmail.com>
2026-08-25 01:47:31 +0530

    chore: update lualine

--- config/plugins/lualine.nix
+++ config/plugins/lualine.nix
@@ -1,13 +1,12 @@
-{ ... }:
+{ lib, ... }:
 let
   colors = {
-    nord1 = "#3B4252";
-    nord3 = "#4C566A";
-    nord5 = "#E5E9F0";
-    nord6 = "#ECEFF4";
-    nord7 = "#8FBCBB";
-    nord9 = "#81a1c1";
+    /*nixfmt:disable*/
+    nord1 = "#3B4252"; nord3 = "#4C566A";
+    nord5 = "#E5E9F0"; nord6 = "#ECEFF4";
+    nord7 = "#8FBCBB"; nord9 = "#81a1c1";
     nord13 = "#EBCB8B";
+    /*nixfmt:enable*/
   };
 in
 {
@@ -18,91 +17,89 @@ in
         options = {
           icons_enabled = true;
           theme = {
+            /*nixfmt:disable*/
             normal = {
-              a = {
-                fg = colors.nord1;
-                bg = colors.nord9;
-              };
-              b = {
-                fg = colors.nord5;
-                bg = colors.nord1;
-              };
-              c = {
-                fg = colors.nord5;
-                bg = colors.nord3;
-              };
+              a = { fg = colors.nord1; bg = colors.nord9; };
+              b = { fg = colors.nord5; bg = colors.nord1; };
+              c = { fg = colors.nord5; bg = colors.nord3; };
             };
-            insert.a = {
-              fg = colors.nord1;
-              bg = colors.nord6;
-              gui = "bold";
-            };
-            visual.a = {
-              fg = colors.nord1;
-              bg = colors.nord7;
-              gui = "bold";
-            };
-            replace.a = {
-              fg = colors.nord1;
-              bg = colors.nord13;
-              gui = "bold";
-            };
+            insert.a = { fg = colors.nord1; bg = colors.nord6; gui = "bold"; };
+            visual.a = { fg = colors.nord1; bg = colors.nord7; gui = "bold"; };
+            replace.a = { fg = colors.nord1; bg = colors.nord13; gui = "bold"; };
             inactive = {
-              a = {
-                fg = colors.nord5;
-                bg = colors.nord1;
-              };
-              b = {
-                fg = colors.nord5;
-                bg = colors.nord1;
-              };
-              c = {
-                fg = colors.nord5;
-                bg = colors.nord1;
-              };
+              a = { fg = colors.nord5; bg = colors.nord1; };
+              b = { fg = colors.nord5; bg = colors.nord1; };
+              c = { fg = colors.nord5; bg = colors.nord1; };
             };
-          };
-          component_separators = {
-            left = "";
-            right = "";
-          };
-          section_separators = {
-            left = "";
-            right = "";
+            /*nixfmt:enable*/
           };
           disabled_filetypes = {
             statusline = [ ];
             tabline = [ ];
             winbar = [ ];
           };
-          ignore_focus = [ ];
-          always_divide_middle = true;
-          globalstatus = true;
-          refresh = {
-            statusline = 1000;
-            tabline = 1000;
-            winbar = 1000;
-          };
         };
         sections = {
           lualine_a = [ "mode" ];
           lualine_b = [
-            "branch"
-            "diff"
-            "diagnostics"
-          ];
-          lualine_c = [ "filename" ];
-          lualine_x = [
-            "encoding"
-            "lsp"
-            "fileformat"
             {
-              "@1" = "filetype";
-              icons_enabled = false;
+              "@1" = "branch";
+              icon = " •";
+            }
+            "diff"
+            {
+              "@1" = "diagnostics";
+              symbols = {
+                error = "󰅙  ";
+                warn = "  ";
+                info = "  ";
+                hint = "󰌵 ";
+              };
             }
           ];
-          lualine_y = [ "progress" ];
-          lualine_z = [ "location" ];
+          lualine_c = [
+            {
+              "@1" = "filename";
+              symbols = {
+                modified = " ";
+                readonly = " ";
+              };
+            }
+          ];
+          lualine_x = [
+            {
+              "@1" = lib.generators.mkLuaInline ''
+                function()
+                  local buf_ft = vim.bo.filetype
+                  local excluded_buf_ft = { toggleterm = true, NvimTree = true, ["neo-tree"] = true, TelescopePrompt = true }
+
+                  if excluded_buf_ft[buf_ft] then
+                    return ""
+                    end
+
+                  local bufnr = vim.api.nvim_get_current_buf()
+                  local clients = vim.lsp.get_clients({ bufnr = bufnr })
+
+                  if vim.tbl_isempty(clients) then
+                    return "No Active LSP"
+                  end
+
+                  local active_clients = {}
+                  for _, client in ipairs(clients) do
+                    table.insert(active_clients, client.name)
+                  end
+
+                  return table.concat(active_clients, ", ")
+                end
+              '';
+              icon = " ";
+            }
+          ];
+          lualine_y = [
+            "fileformat"
+            "filetype"
+          ];
+          lualine_z = [ "progress" ];
         };
         inactive_sections = {
           lualine_a = [ ];
@@ -116,7 +113,17 @@ in
           lualine_a = [
             {
               "@1" = "buffers";
+              symbols = {
+                modified = " ●";
+                alternate_file = "";
+                directory = "󰉋 ";
+              };
               icons_enabled = false;
+              show_modified_status = true;
+              component_separators = {
+                left = "•";
+                right = "•";
+              };
             }
           ];
           lualine_b = [ ];
